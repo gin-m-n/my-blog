7f649127043e10711fe0b77f6cf7cf3e24b6a1a3
takaodaze <gin2798@gmail.com>
2025-04-19 06:10:01 +0900

    minimum impl

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,14 +1,43 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { marked, Renderer } from 'marked';
+import matter from 'gray-matter';
+
+const toc: { anchor: string; level: number; text: string }[] = [];
+
+const renderer = new Renderer();
+renderer.heading = function ({ tokens, depth }) {
+	const text = this.parser.parseInline(tokens);
+	const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+	toc.push({
+		anchor: escapedText,
+		level: depth,
+		text: text
+	});
+
+	return `  <h${depth}>
+              <a name="${escapedText}" class="anchor" href="#${escapedText}">
+                ${text}
+              </a>
+            </h${depth}>`;
+};
+
+marked.use({
+	renderer: renderer
+});
 
 export const load: PageServerLoad = async ({ params }) => {
 	try {
-		const { toc, html, attributes } = await import(`@articles/${params.slug}.md`);
-    console.log(toc);
+		const { default: raw } = await import(`@articles/${params.slug}.md?raw`);
+		const { content, data: attributes } = matter(raw);
+
+		const md = await marked.parse(content);
+
 		return {
-			attributes: attributes as Record<string, unknown>,
-			content: html as string,
-			toc: toc as { level: string; content: string }[]
+			attributes: attributes,
+			content: md,
+			toc: toc
 		};
 	} catch {
 		error(404, { message: 'Not Found' });
